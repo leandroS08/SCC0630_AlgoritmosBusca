5f3facc0623657158387b265d985a01bfda67dbd
leandroS08 <leandroasilva.br@gmail.com>
2021-06-18 16:44:08 -0300

    Second commit

--- knn.py
+++ knn.py
@@ -9,12 +9,14 @@ lista_vertices = []
 d_max = 10
 
 def main():
-    v = 5
-    k = 1
+    v = 10
+    k = 4
     grafo_knn(v, k)
-    rota = [1, 2, 3, 4, 0]
+    rota = list(range(v))
     coordenadas = np.array(lista_vertices)
-    plota_resultado(rota, coordenadas)
+    print(rota)
+    print(coordenadas)
+    # plota_resultado(rota, coordenadas)
 
 
 def grafo_knn(v, k):
@@ -29,20 +31,33 @@ def grafo_knn(v, k):
     # TODO: gera matrix KNN de distâncias
     A = kneighbors_graph(lista_vertices, 2, mode='distance', include_self=False)
     array = A.toarray()
+    g = nx.Graph()
+
+    for x in range(len(array)):
+        for y in range(len(array[x])):
+            if array[x][y] != 0.0:
+                g.add_edge(str(x),str(y),weight=array[x][y])
+
+    nx.draw_networkx(g,alpha=0.6,node_size=70,font_size=5)
+    plt.show()
+
     print(A)
     print(array)
+    coordenadas = np.array(lista_vertices)
+
+    # plota_resultado(array, coordenadas)
+
 
 def plota_resultado(rota, coordenadas):
-    d_plot = np.concatenate((np.array([coordenadas[rota[i]] for i in range(len(rota))]),np.array([coordenadas[rota[0]]])))
+    # d_plot = np.concatenate((np.array([coordenadas[rota[i]] for i in range(len(rota))]),np.array([coordenadas[rota[0]]])))
 
     plt.scatter(coordenadas[:,0],coordenadas[:,1])
 
-    plt.plot(d_plot[:,0],d_plot[:,1])
+    # plt.plot(d_plot[:,0],d_plot[:,1])
 
-    for i, txt in enumerate(rota):
-        plt.annotate(str(rota[i]), coordenadas[rota[i]])
+    # for i, txt in enumerate(rota):
+    #     plt.annotate(str(rota[i]), coordenadas[rota[i]])
 
     plt.show()
 
-if __name__ == '__main__':
-    main()
+main()
