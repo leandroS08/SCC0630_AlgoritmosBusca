2abb3d569822b61e989062bd387bad6e43f6f13c
leandroS08 <leandroasilva.br@gmail.com>
2021-06-23 17:43:49 -0300

    Começando algoritmo A

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from grafos import *
 from busca_largura import *
+from algoritmo_a import *
 
 def main():
 
@@ -19,8 +20,10 @@ def main():
     #print(rota)
 
     rota1 = busca_largura(M, v1, v2)
+    rota2 = busca_largura(M, v1, v2)
 
     print("\nRota (busca em largura):", rota1)
+    print("Rota (algoritmo A):", rota2)
 
     plota_grafo(M, lista_vertices)
 
