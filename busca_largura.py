7897ebe18ade3ddbf925b411316bdadbd1fcbdba
leandroS08 <leandroasilva.br@gmail.com>
2021-06-24 00:08:54 -0300

    Consertando busca em largura

--- busca_largura.py
+++ busca_largura.py
@@ -14,23 +14,70 @@ def busca_largura(M, v1, v2):
 
         vertice = fila.pop(0)
 
-        linha_aux = []
-        for i in range(len(M)):
-            linha_aux.append([i, M[vertice][i]])
-        
-        linha_aux.sort(key=lambda tup: tup[1])
-        
-        #print("\nLinha:")
-        #print(linha_aux)   
+        if(M[vertice][v2] != 0):
+            visitados[v2] = 1
+            rotas[v2] = rotas[vertice].copy()
+            rotas[v2].append(v2)
+            fila.append(v2)
+            break
+        else:
+            for i in range(len(M)):
+                if(M[vertice][i] != 0):
+                    if(visitados[i] == 0):
+                        visitados[i] = 1
+                        rotas[i] = rotas[vertice].copy()
+                        rotas[i].append(i)
+                        fila.append(i)
 
-        for i in range(len(M)):
-            if(linha_aux[i][1] != 0):
-                j = linha_aux[i][0]
-                if(visitados[j] == 0):
-                    visitados[j] = 1
-                    rotas[j] = rotas[vertice].copy()
-                    rotas[j].append(j)
-                    fila.append(j)
+        #print("\n Visitas:")
+        #print(visitados)
+
+        #print("\n Rotas:") 
+        #print(rotas)
+
+    return rotas[v2]
+
+
+def busca_largura_otimizado(M, v1, v2):
+    fila = []
+    visitados = [ 0 for i in range(len(M)) ]
+    rotas = [ [] for i in range(len(M))]
+    for i in range(len(M)):
+        rotas[i].append(v1)
+
+    fila.append(v1)
+    visitados[v1] = 1
+
+    while len(fila) > 0:
+        #print("\n Fila:")
+        #print(fila)
+
+        vertice = fila.pop(0)
+
+        if(M[vertice][v2] != 0):
+            visitados[v2] = 1
+            rotas[v2] = rotas[vertice].copy()
+            rotas[v2].append(v2)
+            fila.append(v2)
+            break
+        else:
+            linha_aux = []
+            for i in range(len(M)):
+                linha_aux.append([i, M[vertice][i]])
+            
+            linha_aux.sort(key=lambda tup: tup[1])
+            
+            #print("\nLinha:")
+            #print(linha_aux)   
+
+            for i in range(len(M)):
+                if(linha_aux[i][1] != 0):
+                    j = linha_aux[i][0]
+                    if(visitados[j] == 0):
+                        visitados[j] = 1
+                        rotas[j] = rotas[vertice].copy()
+                        rotas[j].append(j)
+                        fila.append(j)
 
         #print("\n Visitas:")
         #print(visitados)
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     v = 10
     k = 5
 
-    gera_grafo_knn(v, k)
+    #gera_grafo_knn(v, k)
 
     #NOTE: Lê grafo predefinidos armazenados em arquivos
     pos, M = le_grafo_knn()
@@ -24,16 +24,16 @@ def main():
     #print("\nRota:")
     #print(rota)
 
-    #rota1 = busca_largura(M, v1, v2)
+    rota1 = busca_largura_otimizado(M, v1, v2)
     #rota2 = busca_largura(M, v1, v2)
 
     #NOTE: Busca em profundidade
-    rota = busca_profundidade(M, v1, v2)
-    print("\nRota (busca em profundidade):", rota)
+    rota3 = busca_profundidade(M, v1, v2)
+    print("\nRota (busca em profundidade):", rota3)
 
-    #print("\nRota (busca em largura):", rota1)
+    print("\nRota (busca em largura):", rota1)
     #print("Rota (algoritmo A):", rota)
 
-    plota_grafo(M, pos, rota)
+    plota_grafo(M, pos, rota1)
 
 main()
