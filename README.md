057f8840834a40d3524d3194142e98e8fa27e037
leandroS08 <leandroasilva.br@gmail.com>
2021-07-05 19:16:02 -0300

    Versão final de teste

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # SCC0630_AlgoritmosBusca
+
+Repositório com códigos para o Trabalho 2 da disciplina de Inteligência Artificial - SCC0630.
+
+Alunos: Leandro Antonio Silva (9805341) e Maria Luisa do Nascimento da Silva (10310721).
+
+Instalar as seguintes bibliotecas de Python: sklearn, matplotlib, networkx, numpy e collections. Para isso, usar o comando:
+
+pip3 install -U <nome_biblioteca>
+
+Para executar o código:
+
+python3 main.py
+
+O único input requisitado pelo código é o indice do teste a ser realizado, um número inteiro de 1 a 9. As especificações de cada teste estão no relatório do projeto.
--- main.py
+++ main.py
@@ -6,9 +6,27 @@ from best_first_search import best_first_search
 from buscas_a import *
 
 def main():
-    indice = 1
-    v = 500
-    k = 3
+    print("\n------------- ALGORITMOS DE BUSCA EM GRAFOS -------------")
+
+    indice = int(input("\n Digite o indice do teste a ser realizado (1 a 9):"))
+    while( indice > 9 or indice <= 0 ):
+        indice = int (input("   > Entrada invalida, digite novamente:"))
+    
+    with open('testes.txt') as f:
+        content = f.readlines()[indice]
+
+    leitura = content.split(" ")
+
+    v = int(leitura[1])
+    k = int(leitura[2])
+    v1 = int(leitura[3])
+    v2 = int(leitura[4])
+
+    print("\n Informações do grafo:")
+    print("   > numero de vertices (v):", v)
+    print("   > numero de vizinhos (k):", k)
+    print("   > vertice de origem  (v1):", v1)
+    print("   > vertice de destino (v2):", v2)
 
     #gera_grafo_knn(v, k, indice)
 
@@ -23,23 +41,19 @@ def main():
     #print("\nMatriz:")
     #print (M)
 
-    v1 = 0
-    v2 = 138
-
-    print("\nVertice origem:", v1, " || Vertice destino ", v2)
-
     #rota = list(range(v))
     #print("\nRota:")
     #print(rota)
 
-    ti = time.time()
-    rota1 = busca_profundidade(M, v1, v2)
-    dt = time.time() - ti
-    print("\nBusca em profundidade")
-    print("   > Rota:", rota1)
-    print("   > Distancia: {:.4f}".format(dist_rota(M, rota1)))
-    print("   > Tempo: {:.4f}".format(dt))
-    plota_grafo(M, pos, rota1, v1, v2, 1, "Busca em Profundidade")
+    if( indice <= 3):
+        ti = time.time()
+        rota1 = busca_profundidade(M, v1, v2)
+        dt = time.time() - ti
+        print("\nBusca em profundidade")
+        print("   > Rota:", rota1)
+        print("   > Distancia: {:.4f}".format(dist_rota(M, rota1)))
+        print("   > Tempo: {:.4f}".format(dt))
+        plota_grafo(M, pos, rota1, v1, v2, 1, "Busca em Profundidade")
 
     ti = time.time()
     rota2 = busca_largura(M, v1, v2)
